786434ac29f7cb4368637f0046a86503f1985c7b
Pratikbhor2004 <pratikbhor2004@gmail.com>
2026-03-29 02:57:19 +0530

    1st commit

--- AirlineManagementSystem/src/airlinemanagementsystem/Conn.java
+++ AirlineManagementSystem/src/airlinemanagementsystem/Conn.java
@@ -9,12 +9,23 @@ public class Conn {
     
     public Conn() {
         try {
+            // Load MySQL Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "codeforinterview");
+
+            // Correct database name + URL
+            c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/airlinemanagementsystem",
+                "root",
+                "Pradip@123"   // keep "" if no password, otherwise put your password
+            );
+
+            // Create Statement
             s = c.createStatement();
+
+            System.out.println("Database Connected Successfully!");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
